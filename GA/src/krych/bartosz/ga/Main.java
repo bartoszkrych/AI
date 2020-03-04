f6534996eab1828ec4506b3e8eef565db8e121d2
bartosz.krych <bartuskr@gmail.com>
2020-03-04 15:45:29 +0100

    [GA] Comment code

--- GA/src/krych/bartosz/ga/Main.java
+++ GA/src/krych/bartosz/ga/Main.java
@@ -3,7 +3,6 @@ package krych.bartosz.ga;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.Scanner;
 import java.util.regex.Pattern;
@@ -12,27 +11,28 @@ public class Main {
 
     public static void main(String[] args) {
 
-        getData();
+        GeneticAlgorithm ga = new GeneticAlgorithm(123, getData());
+
     }
 
-    private static List<City> randomRoute(List<City> cities) {
-        List<Integer> gen = new ArrayList<>();
-        for (int i = 0; i < cities.size(); i++) {
-            gen.add(i);
-        }
-        Collections.shuffle(gen);
+//    private static List<City> randomRoute(List<City> cities) {
+//        List<Integer> gen = new ArrayList<>();
+//        for (int i = 0; i < cities.size(); i++) {
+//            gen.add(i);
+//        }
+//        Collections.shuffle(gen);
+//
+//        Integer[] genInt = new Integer[cities.size()];
+//        gen.toArray(genInt);
+//
+//        List<City> result = new ArrayList<>();
+//        for (int i = 0; i < gen.size(); i++) {
+//            result.add(cities.get(genInt[i]));
+//        }
+//        return result;
+//    }
 
-        Integer[] genInt = new Integer[cities.size()];
-        gen.toArray(genInt);
-
-        List<City> result = new ArrayList<>();
-        for (int i = 0; i < gen.size(); i++) {
-            result.add(cities.get(genInt[i]));
-        }
-        return result;
-    }
-
-    private static void getData() {
+    private static City[] getData() {
         File file = new File("TSP/berlin11_modified.tsp");
         List<City> cities = new ArrayList<>();
 
@@ -42,7 +42,7 @@ public class Main {
 
         try (Scanner scanner = new Scanner(file)) {
             while (scanner.hasNextLine()) {
-                line= scanner.nextLine();
+                line = scanner.nextLine();
                 if (line.equals("EOF")) break;
                 if (startCities) {
                     Pattern pattern = Pattern.compile(" ");
@@ -59,5 +59,8 @@ public class Main {
         } catch (IOException e) {
             e.printStackTrace();
         }
+        City[] arrCities = new City[cities.size()];
+        cities.toArray(arrCities);
+        return arrCities;
     }
 }
