f115cc45dedab874dc94f07be9424d2e37ac83f8
bartosz.krych <bartuskr@gmail.com>
2020-03-08 13:02:28 +0100

    [GA] Individual - Integer[]

--- GA/src/krych/bartosz/ga/Individual.java
+++ GA/src/krych/bartosz/ga/Individual.java
@@ -1,53 +1,26 @@
 package krych.bartosz.ga;
 
-import java.text.DecimalFormat;
-
 public class Individual {
-    private City[] route;
+    private Integer[] route;
     private Double distance;
-    private Double fitness;
 
-    public Individual(City[] route) {
+    public Individual(Integer[] route, TSPProblem tsp) {
         this.route = route;
-        distance = 0D;
-        fitness = 0D;
-        getFitness();
+        distance = tsp.getDistance(this);
     }
 
     public Double getDistance() {
-        if (distance == 0) {
-            Double pathDist = 0D;
-            City fromCity;
-            City toCity;
-            for (int i = 0; i < route.length; i++) {
-                fromCity = route[i];
-                if (i + 1 < route.length)
-                {
-                    toCity = route[i+1];
-                } else {
-                    toCity = route[0];
-                }
-                pathDist += fromCity.distance(toCity);
-            }
-            distance = pathDist;
-        }
         return distance;
     }
 
-    public Double getFitness() {
-        if (fitness == 0) {
-            fitness = 1 / getDistance();
-        }
-        return fitness;
+    public int getCityByIdx(Integer idx) {
+        return route[idx];
     }
 
     @Override
     public String toString() {
-        DecimalFormat df = new DecimalFormat("#.#########");
-
         return "Individual{" +
                 "distance=" + distance +
-                ", fitness=" + df.format(fitness) +
                 '}';
     }
 }
