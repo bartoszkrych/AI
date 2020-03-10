5319aece8b50953242033fe0e4ac721b3e8a455b
bartosz.krych <bartuskr@gmail.com>
2020-03-10 11:01:28 +0100

    [GA] Roulette Selection

--- GA/src/krych/bartosz/ga/GeneticAlgorithm.java
+++ GA/src/krych/bartosz/ga/GeneticAlgorithm.java
@@ -5,6 +5,7 @@ import krych.bartosz.ga.individual.Individual;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class GeneticAlgorithm {
     private TSPProblem tspProblem;
@@ -12,6 +13,8 @@ public class GeneticAlgorithm {
     private int posSize = 5;
 
     private int indiAlgNumber = 10;
+
+    private int reproductSize = 10;
 
 
     public GeneticAlgorithm(TSPProblem tspProblem, AlgorithmIndividual algorithmIndividual) {
@@ -28,11 +31,32 @@ public class GeneticAlgorithm {
     public List<Individual> initPopulation() {
         List<Individual> population = new ArrayList<>();
         for (int i = 0; i < posSize; i++) {
-            population.add(algorithmIndividual.getNewIndividual(10));
+            population.add(algorithmIndividual.getNewIndividual(indiAlgNumber));
         }
-
         return population;
     }
 
+    public List<Individual> selection(List<Individual> population) {
+        List<Individual> selected = new ArrayList<>();
+        for (int i = 0; i < reproductSize; i++) {
+            selected.add(rouletteSelection(population));
+        }
+        return selected;
+    }
+
+    public Individual rouletteSelection(List<Individual> population) {
+        double totalFit = population.stream().map(Individual::getFitness).mapToDouble(Double::doubleValue).sum();
+        Random random = new Random();
+        double selectedVal = random.nextDouble() * totalFit;
+        double recVal = 1 / selectedVal;
+        double curSum = 0D;
+        for (Individual i : population) {
+            curSum += 1 / i.getFitness();
+            if (curSum >= recVal) {
+                return i;
+            }
+        }
+        return population.get(random.nextInt(posSize));
+    }
 
 }
