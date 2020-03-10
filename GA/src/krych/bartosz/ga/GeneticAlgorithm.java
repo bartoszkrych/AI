fd8cdb2e27172bc545c7834aee7d071cf353532a
bartosz.krych <bartuskr@gmail.com>
2020-03-10 10:43:00 +0100

    [GA] Initial population

--- GA/src/krych/bartosz/ga/GeneticAlgorithm.java
+++ GA/src/krych/bartosz/ga/GeneticAlgorithm.java
@@ -1,10 +1,17 @@
 package krych.bartosz.ga;
 
 import krych.bartosz.ga.individual.AlgorithmIndividual;
+import krych.bartosz.ga.individual.Individual;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class GeneticAlgorithm {
     private TSPProblem tspProblem;
     private AlgorithmIndividual algorithmIndividual;
+    private int posSize = 5;
+
+    private int indiAlgNumber = 10;
 
 
     public GeneticAlgorithm(TSPProblem tspProblem, AlgorithmIndividual algorithmIndividual) {
@@ -16,6 +23,15 @@ public class GeneticAlgorithm {
         for (int i = 0; i < tspProblem.getCountCity(); i++) {
             System.out.println(algorithmIndividual.getNewIndividual(i + 1 % tspProblem.getCountCity()).toString());
         }
+    }
+
+    public List<Individual> initPopulation() {
+        List<Individual> population = new ArrayList<>();
+        for (int i = 0; i < posSize; i++) {
+            population.add(algorithmIndividual.getNewIndividual(10));
+        }
+
+        return population;
     }
 
 
