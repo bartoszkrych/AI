594e362ddb285474768d4ca637b378b10814e8b9
bartosz.krych <bartuskr@gmail.com>
2020-03-11 19:07:12 +0100

    [GA] Change Integer to int and toString()

--- GA/src/krych/bartosz/ga/individual/Individual.java
+++ GA/src/krych/bartosz/ga/individual/Individual.java
@@ -2,31 +2,34 @@ package krych.bartosz.ga.individual;
 
 import krych.bartosz.ga.TSPProblem;
 
+import java.util.Arrays;
+
 public class Individual {
-    private Integer[] genotype;
+    private int[] genotype;
     private Double fitness;
 
-    public Individual(Integer[] route, TSPProblem tsp) {
+    public Individual(int[] route, TSPProblem tsp) {
         this.genotype = route;
-        this.fitness = tsp.getDistance(this);
+        this.fitness = tsp.getDistance(route);
     }
 
     public Double getFitness() {
         return fitness;
     }
 
-    public int getCityByIdx(Integer idx) {
+    public int getCityByIdx(int idx) {
         return genotype[idx];
     }
 
-    public Integer[] getGenotype() {
+    public int[] getGenotype() {
         return genotype;
     }
 
     @Override
     public String toString() {
-        return "Individual{" +
-                "fitness=" + fitness +
+        return "{" +
+                "genotype=" + Arrays.toString(genotype) +
+                ", fitness=" + fitness +
                 '}';
     }
 }
