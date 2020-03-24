cbeab27df1b750baed0b38b2106f3012ddcc71a9
bartosz.krych <bartuskr@gmail.com>
2020-03-24 21:37:39 +0100

    [CSP] Change constructor

--- CSP/src/krych/bartosz/Crossword.java
+++ CSP/src/krych/bartosz/Crossword.java
@@ -3,8 +3,11 @@ package krych.bartosz;
 import java.util.List;
 
 public class Crossword {
-    private char[] domain;
+    private String[] domain;
     private char[][] board;
 
-
+    public Crossword(String[] domain, char[][] board) {
+        this.domain = domain;
+        this.board = board;
+    }
 }
