ca6c46b289427b82af2a611aea5f379a476c558e
bartosz.krych <bartuskr@gmail.com>
2020-03-24 16:22:49 +0100

    [CSP] Add Variable

---
+++ CSP/src/krych/bartosz/Variable.java
@@ -0,0 +1,28 @@
+package krych.bartosz;
+
+import java.util.List;
+
+public class Variable {
+    private int value;
+    private List<Integer> domain;
+
+    public Variable(List<Integer> domain) {
+        this.value = 0;
+        this.domain = domain;
+    }
+
+    public Variable(int value) {
+        this.value = value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+        domain = null;
+    }
+
+    public void setDomain(List<Integer> domain) {
+        this.domain = domain;
+    }
+
+
+}
