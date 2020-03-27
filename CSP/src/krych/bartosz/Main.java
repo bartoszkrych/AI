49addfacf5f063323ca4ac3ca39a9c5467eb83ae
bartosz.krych <bartuskr@gmail.com>
2020-03-27 14:49:44 +0100

    [CSP] cleaning

--- CSP/src/krych/bartosz/Main.java
+++ CSP/src/krych/bartosz/Main.java
@@ -13,11 +13,11 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        List<Sudoku> s = getSudokuFromFile();
-        // select a sudoku to testing
-        s.get(0).printBoard();
-        Backtracking backtrackingSudoku = new Backtracking(s.get(0));
-        backtrackingSudoku.start();
+//        List<Sudoku> s = getSudokuFromFile();
+//        select a sudoku to testing
+//        s.get(0).printBoard();
+//        Backtracking backtrackingSudoku = new Backtracking(s.get(0));
+//        backtrackingSudoku.start();
 
         // or all sudokus
         /*for(Sudoku sudoku : s)
@@ -38,41 +38,6 @@ public class Main {
             crossword.printBoard();
             new Backtracking(crossword).start();
         }
-    }
-
-    private static List<Sudoku> getSudokuFromFile() {
-        File file = new File("Files/Sudoku.csv");
-        List<Sudoku> sudokus = new ArrayList<>();
-        String[] readString;
-        String line;
-
-        try (Scanner scanner = new Scanner(file)) {
-            scanner.nextLine();
-            while (scanner.hasNextLine()) {
-                line = scanner.nextLine();
-
-                readString = line.split(";");
-
-                int x = 0;
-                int y = 0;
-                SudokuVariable[][] board = new SudokuVariable[9][9];
-
-                for (int i = 0; i < readString[2].length(); i++) {
-                    board[y][x++] = readString[2].charAt(i) == '.' ? new SudokuVariable(y, x) :
-                            new SudokuVariable(Character.getNumericValue(readString[2].charAt(i)), y, x);
-                    if (x == 9) {
-                        x = 0;
-                        y++;
-                    }
-                }
-                sudokus.add(new Sudoku(board, readString[1]));
-
-            }
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return sudokus;
     }
 
     private static Crossword getCrosswordFromFile(int fileNumber) {
@@ -108,5 +73,40 @@ public class Main {
         }
         Character[][] arrBord = board.stream().map(u -> u.toArray(new Character[0])).toArray(Character[][]::new);
         return new Crossword(words, arrBord);
+    }
+
+    private static List<Sudoku> getSudokuFromFile() {
+        File file = new File("Files/Sudoku.csv");
+        List<Sudoku> sudokus = new ArrayList<>();
+        String[] readString;
+        String line;
+
+        try (Scanner scanner = new Scanner(file)) {
+            scanner.nextLine();
+            while (scanner.hasNextLine()) {
+                line = scanner.nextLine();
+
+                readString = line.split(";");
+
+                int x = 0;
+                int y = 0;
+                SudokuVariable[][] board = new SudokuVariable[9][9];
+
+                for (int i = 0; i < readString[2].length(); i++) {
+                    board[y][x++] = readString[2].charAt(i) == '.' ? new SudokuVariable(y, x) :
+                            new SudokuVariable(Character.getNumericValue(readString[2].charAt(i)), y, x);
+                    if (x == 9) {
+                        x = 0;
+                        y++;
+                    }
+                }
+                sudokus.add(new Sudoku(board, readString[1]));
+
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return sudokus;
     }
 }
