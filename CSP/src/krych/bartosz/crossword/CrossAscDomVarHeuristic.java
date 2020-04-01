ab3237e82d5bf16e2ddb31b0ec30095573e1582a
bartosz.krych <bartuskr@gmail.com>
2020-04-01 12:49:07 +0200

    [CSP] VarHeuristics - rename

--- CSP/src/krych/bartosz/crossword/CrossAscDomVarHeuristic.java
+++ CSP/src/krych/bartosz/crossword/CrossAscDomVarHeuristic.java
@@ -2,13 +2,14 @@ package krych.bartosz.crossword;
 
 import krych.bartosz.abstra.Heuristic;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
-public class CrossDescVarHeuristic implements Heuristic<CrosswordVariable> {
+public class CrossAscDomVarHeuristic implements Heuristic<CrosswordVariable> {
 
     @Override
     public List<CrosswordVariable> sort(List<CrosswordVariable> list) {
-        return list.stream().sorted((x, y) -> -x.getLength().compareTo(y.getLength())).collect(Collectors.toList());
+        return list.stream().sorted(Comparator.comparingInt(x -> x.getDomain().size())).collect(Collectors.toList());
     }
 }
--- CSP/src/krych/bartosz/crossword/CrossDescLenVarHeuristic.java
+++ CSP/src/krych/bartosz/crossword/CrossDescLenVarHeuristic.java
@@ -2,14 +2,13 @@ package krych.bartosz.crossword;
 
 import krych.bartosz.abstra.Heuristic;
 
-import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
-public class CrossDescDomVarHeuristic implements Heuristic<CrosswordVariable> {
+public class CrossDescLenVarHeuristic implements Heuristic<CrosswordVariable> {
 
     @Override
     public List<CrosswordVariable> sort(List<CrosswordVariable> list) {
-        return list.stream().sorted(Comparator.comparingInt(x -> x.getDomain().size())).collect(Collectors.toList());
+        return list.stream().sorted((x, y) -> -x.getLength().compareTo(y.getLength())).collect(Collectors.toList());
     }
 }
