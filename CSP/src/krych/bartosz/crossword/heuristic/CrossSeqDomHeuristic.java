29fc2a1b51a2812055cf55b12f1117eea50c0a9b
bartosz.krych <bartuskr@gmail.com>
2020-04-01 17:23:53 +0200

    [CSP] Add class

---
+++ CSP/src/krych/bartosz/crossword/heuristic/CrossSeqDomHeuristic.java
@@ -0,0 +1,12 @@
+package krych.bartosz.crossword.heuristic;
+
+import krych.bartosz.abstra.DomHeuristic;
+import krych.bartosz.crossword.CrosswordVariable;
+
+import java.util.List;
+
+public class CrossSeqDomHeuristic implements DomHeuristic<CrosswordVariable> {
+    @Override
+    public void sort(List<CrosswordVariable> variables) {
+    }
+}
