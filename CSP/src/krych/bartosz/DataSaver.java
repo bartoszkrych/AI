ca2b43eea4dca6ef51ae8e19cb333ae564c49170
bartosz.krych <bartuskr@gmail.com>
2020-04-02 20:01:28 +0200

    [CSP] DataSever for research

---
+++ CSP/src/krych/bartosz/DataSaver.java
@@ -0,0 +1,33 @@
+package krych.bartosz;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.List;
+
+public class DataSaver {
+
+    private String fileName;
+    private String cspType;
+    private String folderName;
+
+    public DataSaver(String fileName, String folderName) {
+        this.fileName = fileName;
+        this.folderName = folderName;
+    }
+
+    public void setCspType(String cspType) {
+        this.cspType = cspType;
+    }
+
+    public void saveToFile(List<String[]> dataLines) {
+        File txtOutputFile = new File(folderName + "/" + fileName + "_" + cspType + ".txt");
+        try (PrintWriter pw = new PrintWriter(txtOutputFile)) {
+            dataLines.stream()
+                    .map(s -> String.join(";", s))
+                    .forEach(pw::println);
+        } catch (FileNotFoundException e) {
+            System.err.println(e.getMessage());
+        }
+    }
+}
