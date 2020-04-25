3d8a15b799e2fc85aaf2a4ebc890bd01e8ff933a
bartosz.krych <bartuskr@gmail.com>
2020-04-25 18:15:59 +0200

    [C4] Add main static for GUI

---
+++ Connect4/src/krych/bartosz/gui/MainGame.java
@@ -0,0 +1,13 @@
+package krych.bartosz.gui;
+
+public class MainGame {
+    public static void main(String[] args) {
+//        GUI connect4 = new GUI();
+//        GameParams.gameMode = Consts.HumanVsAi;
+//        GameParams.gameMode = Consts.AiVsAi;
+//        GameParams.maxDepth1 = 4;
+//        GameParams.maxDepth2 = 4;
+
+//        connect4.createNewGame();
+    }
+}
