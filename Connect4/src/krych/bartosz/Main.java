02fc77bb9a0d544fbfe9bd0a09a6d6e59a916f43
bartosz.krych <bartuskr@gmail.com>
2020-04-28 22:27:47 +0200

    [C4] new game via console

--- Connect4/src/krych/bartosz/Main.java
+++ Connect4/src/krych/bartosz/Main.java
@@ -1,84 +1,19 @@
 package krych.bartosz;
 
 import krych.bartosz.classes.Consts;
-import krych.bartosz.classes.Move;
-import krych.bartosz.classes.State;
+import krych.bartosz.classes.GameConsole;
 import krych.bartosz.classes.algorithms.AlphaBeta;
-import krych.bartosz.classes.algorithms.NegaScout;
+import krych.bartosz.classes.algorithms.MinMax;
 import krych.bartosz.classes.functions.ThreeInLineEstFun;
-import krych.bartosz.interfaces.GameAlgorithm;
-
-import java.util.List;
-import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-        State game = new State();
-        int maxDepth = 6;
-        GameAlgorithm aiNS = new NegaScout(maxDepth, Consts.P_1, new ThreeInLineEstFun());
-        GameAlgorithm aiAB = new AlphaBeta(maxDepth, Consts.P_2, new ThreeInLineEstFun());
 
-        game.setLastPlayer(Consts.P_2);
+        new GameConsole().startAiVsAi(
+                new MinMax(7, Consts.P_1, new ThreeInLineEstFun()),
+                new AlphaBeta(7, Consts.P_2, new ThreeInLineEstFun()),
+                true);
 
-        printBoard(game.getBoard());
-
-        Scanner in = new Scanner(System.in);
-        int col;
-        while (!game.isEnd()) {
-            switch (game.getLastPlayer()) {
-                case 1:
-                    System.out.println("Round for AI...");
-                    Move aiMove1 = aiNS.findMove(game);
-                    game.nextMove(aiMove1.getCol(), Consts.P_2);
-                    break;
-                case 2:
-                    try {
-                        do {
-                            System.out.print("\nSelect column: ");
-                            col = in.nextInt();
-                        } while (game.isFullCol(col - 1));
-                    } catch (Exception e) {
-                        System.err.println("Only [1,7].\n");
-                        break;
-                    }
-                    game.nextMove(col - 1, Consts.P_1);
-                    System.out.println();
-//                    System.out.println("Round for AI...");
-//                    Move aiMove2 = aiAB.findMove(game);
-//                    game.nextMove(aiMove2.getCol(), Consts.P_1);
-                    break;
-                default:
-                    break;
-            }
-            printBoard(game.getBoard());
-        }
-        in.close();
-
-        System.out.println();
-        if (game.getWinner().equals(Consts.P_1)) {
-            System.out.println("Player 1 wins!");
-        } else if (game.getWinner().equals(Consts.P_2)) {
-            System.out.println("Player 2 wins!");
-        } else {
-            System.out.println("It's a draw!");
-        }
-    }
-
-
-    public static void printBoard(List<List<Integer>> board) {
-        System.out.println("| 1 | 2 | 3 | 4 | 5 | 6 | 7 | \n");
-        for (int i = 0; i < 6; i++) {
-            for (int j = 0; j < 7; j++) {
-                if (board.get(i).get(j).equals(Consts.P_1)) {
-                    System.out.print("| 1 ");
-                } else if (board.get(i).get(j).equals(Consts.P_2)) {
-                    System.out.print("| 2 ");
-                } else {
-                    System.out.print("|   ");
-                }
-            }
-            System.out.print("|\n");
-        }
     }
 }
