05916407f339ac4b3be54679d04843f874913078
bartosz.krych <bartuskr@gmail.com>
2020-05-19 19:41:49 +0200

    [TC] - problem with apostrophe

---
+++ TextClassification/src/Main.java
@@ -0,0 +1,66 @@
+import java.io.*;
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        Set<String> classes = new HashSet<>();
+        List<String> contents = new ArrayList<>();
+        List<String> toSave = new ArrayList<>();
+        String arg1 = "@RELATION wikis\n\n@ATTRIBUTE wiki  STRING\n@ATTRIBUTE class    ";
+        String arg4 = "@DATA \n";
+
+
+        File[] files = new File("data/test").listFiles();
+        //If this pathname does not denote a directory, then listFiles() returns null.
+
+        for (File file : files) {
+            if (file.isFile()) {
+                String name = file.getName().split("_")[0];
+                classes.add(name);
+
+                try {
+                    BufferedReader br = new BufferedReader(new FileReader(file.getPath()));
+                    StringBuilder sb = new StringBuilder();
+                    String line = br.readLine();
+
+                    while (line != null) {
+                        sb.append(line);
+                        line = br.readLine();
+                    }
+                    String everything = sb.toString();
+                    everything.replace("nbsp;", " ").replaceAll("\""," ")
+                            .replace(System.getProperty("line.separator"), "").strip();
+
+                    String content = new StringBuilder().append("\"").append(everything).append("\",").append(name).append(System.getProperty("line.separator")).toString();
+                    contents.add(content);
+
+                    br.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        classes.forEach(x -> System.out.println(x));
+
+        StringBuilder builderToSave = new StringBuilder();
+        builderToSave.append(arg1).append("{");
+        List<String> classesList = new ArrayList<>(classes);
+        Collections.sort(classesList);
+        for (int i = 0; i < classes.size(); i++) {
+            builderToSave.append(classesList.get(i));
+            if (i != classes.size() - 1) builderToSave.append(",");
+        }
+        builderToSave.append("}\n\n");
+        builderToSave.append(arg4);
+
+        FileWriter writer = new FileWriter("test.arff");
+        writer.write(builderToSave.toString());
+        for (String str : contents) {
+            writer.write(str);
+        }
+        writer.close();
+    }
+}
